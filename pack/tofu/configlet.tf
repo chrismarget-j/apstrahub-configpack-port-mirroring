4c3ff43f7a16713b69d917fd07b406bb9e0cddb6
Chris Marget <cmarget@juniper.net>
2025-07-31 12:22:39 -0400

    use hostname in configlet name

--- pack/tofu/configlet.tf
+++ pack/tofu/configlet.tf
@@ -3,13 +3,13 @@
 #  SPDX-License-Identifier: MIT
 
 locals {
-  configlet_name = format("in-%s_out-%s->%s",
+  traffic_description = format("in-%s out-%s > %s",
     var.input_ingress_port == null ? "any" : var.input_ingress_port,
     var.input_egress_port == null ? "any" : var.input_egress_port,
     var.output_destination,
   )
   analyzer_allowed_chars = split("", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_")
-  analyzer_name          = join("", [for c in split("", local.configlet_name) : contains(local.analyzer_allowed_chars, c) ? c : "-"])
+  analyzer_name          = join("", [for c in split("", local.traffic_description) : contains(local.analyzer_allowed_chars, c) ? c : "-"])
   output_type            = length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$", var.output_destination)) == 0 ? "interface" : "ip-address"
   ingress_set_cmd        = var.input_ingress_port == null ? "" : format("set forwarding-options analyzer %s input ingress interface %s\n", local.analyzer_name, var.input_ingress_port)
   egress_set_cmd         = var.input_egress_port == null ? "" : format("set forwarding-options analyzer %s input egress interface %s\n", local.analyzer_name, var.input_egress_port)
@@ -18,7 +18,7 @@ locals {
 
 resource "apstra_datacenter_configlet" "example" {
   blueprint_id = var.blueprint_id
-  name         = local.configlet_name
+  name         = format("%s mirror: %s", var.switch_hostname, local.traffic_description)
   condition    = format("hostname in [\"%s\"]", var.switch_hostname)
   generators = [
     {
